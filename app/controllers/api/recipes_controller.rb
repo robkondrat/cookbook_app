f401ba5226dbb3a17d030f78d538debe3b8f3f79
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-01-17 10:56:29 -0600

    update and patch

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -4,8 +4,33 @@ class Api::RecipesController < ApplicationController
     render 'index.json.jb'
   end
 
+  def create
+    @recipe = Recipe.new(
+                          title: params[:title],
+                          chef: params[:chef],
+                          ingredients: params[:ingredients],
+                          directions: params[:directions],
+                          prep_time: params[:prep_time]
+                          )
+    @recipe.save
+    render 'show.json.jb'
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     render 'show.json.jb'
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+
+    @recipe.save
+    render "show.json.jb"
   end
 end
