ad9e52b84017246d8489b40e408399bb6436e356
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-02-14 13:53:44 -0600

    in class js index

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -18,7 +18,8 @@ class Api::RecipesController < ApplicationController
                           title: params[:title],
                           ingredients: params[:ingredients],
                           directions: params[:directions],
-                          prep_time: params[:prep_time]
+                          prep_time: params[:prep_time],
+                          image_url: params[:image_url]
                           )
     @recipe.save
     render 'show.json.jb'
@@ -33,10 +34,10 @@ class Api::RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
 
     @recipe.title = params[:title] || @recipe.title
-    @recipe.chef = params[:chef] || @recipe.chef
     @recipe.ingredients = params[:ingredients] || @recipe.ingredients
     @recipe.directions = params[:directions] || @recipe.directions
     @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.image_url = params[:image_url] || @recipe.image_url
 
     @recipe.save
     render "show.json.jb"
---
+++ public/index.js
@@ -0,0 +1,37 @@
+// response = HTTP.get("http://localhost:3000/api/recipes")
+// recipes = response.parse
+// p recipes
+
+axios.get("http://localhost:3000/api/recipes").then(function(response) {
+  var recipes = response.data;
+  console.log(recipes);
+
+
+  // bonus
+  var axiosRecipesElement = document.querySelector('#axios-recipes');
+  console.log(axiosRecipesElement);
+
+  var message = "";
+
+  recipes.forEach(function(recipe) {
+    message += '<div class="col-md-4 mt-3">';
+      message += '<div class="card">';
+        message += '<div class="card-body">';
+          message += '<h5 class="card-title">' + recipe.title + '</h5>';
+            message += '<p class="card-text">Ingredients: ' + recipe.ingredients + '</p>';
+            message += '<[ class="card-text">Prep Time: ' + recipe.prep_time + '</p>';
+          message += '<a href="/" class="btn btn-primary">More Info</a>';
+        message += '</div>';
+      message += '</div>';
+    message += '</div>';
+  });
+
+  axiosRecipesElement.innerHTML = message;
+});
+
+
+// var response = axios.get("http://localhost:3000/api/recipes");
+// setTimeout(function() {
+//   var recipes = response.data;
+//   console.log(recipes);
+// });
